a3be8b2344e817a7c9a0e2e23e819152051f04f7
Amie Frai <amie.frain@gmail.com>
2022-11-07 14:52:09 +0000

    adding-must-include-file

--- university-post-types.php
+++ university-post-types.php
@@ -1,14 +1,16 @@
 <?php function university_post_types() {
-    register_post_type('event', ['public' => true,
+    register_post_type('event', [
+    'has_archive' => true,
+    'public' => true,
     'show_in_rest' => true,
-    'labels' => ['name' => 'Events', //this array edits how the event labels show in wp admin
-            'add_new_item' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
-            ],
+    'labels' => ['name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
+    ], //this array edits how the event labels show in wp admin
         'menu_icon' => 'dashicons-calendar-alt'
     ]);
 }
 
-add_action('init', 'university_post_types' );
+add_action('init', 'university_post_types');
