1be8864a980c06bc818a577d4fe7bdb4aa92d0b7
Amie Frai <amie.frain@gmail.com>
2022-11-08 15:07:48 +0000

    bug-fixes

--- university-post-types.php
+++ university-post-types.php
@@ -1,17 +1,21 @@
-<?php function university_post_types() {
-    register_post_type('event', [
-    'rewrite' => ['slug' => 'events'],
+<?php
+
+function university_post_types() {
+  register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
-    'show_in_rest' => true,
-    'labels' => ['name' => 'Events',
-        'add_new_item' => 'Add New Event',
-        'edit_item' => 'Edit Event',
-        'all_items' => 'All Events',
-        'singular_name' => 'Event'
-    ], //this array edits how the event labels show in wp admin
-        'menu_icon' => 'dashicons-calendar-alt'
-    ]);
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
 }
 
 add_action('init', 'university_post_types');
