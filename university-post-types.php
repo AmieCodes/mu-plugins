7efb67af82632abcb959878eee53a00697d692ad
Amie Frai <amie.frain@gmail.com>
2022-11-15 13:43:38 +0000

    registeed program page

--- university-post-types.php
+++ university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_post_types() {
+  //Event post type
   register_post_type('event', [
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +16,23 @@ function university_post_types() {
       'singular_name' => 'Event'
     ),
     'menu_icon' => 'dashicons-calendar'
+  ]);
+
+  //Program post type
+  register_post_type('program', [
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards' //taken from wordoress dashicons website
   ]);
 }
 
